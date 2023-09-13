fb5fb0368c1aa41d527af7c872b9b703045afed4
Mi-ay <okunlolafavour16@gmail.com>
2023-09-12 18:07:25 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_times_table -  prints the n times table, starting with 0
+ * @n: parameter on an integer
+ * Return: 0(success)
+ */
+void print_times_table(int n)
+{
+
