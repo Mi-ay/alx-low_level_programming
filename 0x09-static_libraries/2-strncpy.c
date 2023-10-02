67dee771b4dca178dc6b893698639f29c933a2ca
Mi-ay <ayomideokunlola16@gmail.com>
2023-10-02 16:03:53 -0700

    static

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -11,6 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
+
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
@@ -22,5 +23,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
+
+
 	return (dest);
 }
