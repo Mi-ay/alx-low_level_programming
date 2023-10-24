879380607bee651f65ca595ea5cffab62c83a622
Mi-ay <okunlolafavour16@gmail.com>
2023-10-24 15:05:38 -0700

    plop

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,21 +2,23 @@
 
 /**
 * print_listint - prints all the elements of a linked list
-* @h: linked list of type listint_t to print
+* @h: A pointer to the head of the list.
 *
-* Return: number of nodes
+* Return: number of nodes in the list
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+/*assign value to the node*/
+size_t numb = 0;
 
-while (h)
+/*transversing the linked list and printing each member*/
+while (h != NULL)
 {
 printf("%d\n", h->n);
-num++;
+numb++;
 h = h->next;
 }
 
-return (num);
+return (numb);
 }
 
